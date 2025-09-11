f46487ace61271f1fc8b5e01ff594f9da4de2015
mhdraihanr <raihanrafliansyah1234@gmail.com>
2025-09-11 11:07:11 +0700

    fix sidebar ui on mobile

--- obj/Debug/net9.0/blazor-arsip.AssemblyInfo.cs
+++ obj/Debug/net9.0/blazor-arsip.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("blazor-arsip")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+404fd47e5700fa4dd9116e8f618936800b807b2c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36e9345044b975103dfaded7ade3848bb824046f")]
 [assembly: System.Reflection.AssemblyProductAttribute("blazor-arsip")]
 [assembly: System.Reflection.AssemblyTitleAttribute("blazor-arsip")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
